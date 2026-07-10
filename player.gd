2a045f06811af70454723ce0a30ec654384fecda
afanmubashir22 <afanmubashir429@gmail.com>
2026-07-11 00:30:15 +0530

    FIXING ISSUES  WITH RUNNING AND NOT MOVING
    under process

--- player.gd
+++ player.gd
@@ -13,18 +13,17 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
 	
 func _unhandled_input(event: InputEvent) -> void:
-
+	if event.is_action_pressed("ui_cancel"):
+		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+			
+			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
 		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
 		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
-	
-	if event.is_action_pressed("ui_cancel"):
-		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)   
-	else:
-		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-				
+		
 func  _physics_process(delta: float)  -> void:
 					if not is_on_floor():
 						velocity.y -= gravity * delta
