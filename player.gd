3a5b5c864edc2b80462f966d68bdcb66939c2ea7
afanmubashir22 <afanmubashir429@gmail.com>
2026-07-10 23:57:28 +0530

    MOVEMENT SETUP
    I SETED UP THE MOVEMENT LIKE MOVING USING WASD AND ARROW KEYS ESC WITH MOUSES

--- player.gd
+++ player.gd
@@ -10,21 +10,22 @@ extends CharacterBody3D
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-		func _unhandled_input(event: InputEvent) -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
+	
+func _unhandled_input(event: InputEvent) -> void:
 
-if event is InputEventmouseMotion and Input.get_mouse-MODE() == Input.MOUSE_MODE_CAPTURED:
-	rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
-	head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
-	head.rotation.x = clamp(head.rotate.x, deg_to_rad(-80), deg_to_rad(80))
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
+		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 	
 	if event.is_action_pressed("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-			else:
-				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)   
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 				
-				func  _physics_process(delta: float)  -> void:
+func  _physics_process(delta: float)  -> void:
 					if not is_on_floor():
 						velocity.y -= gravity * delta
 						
@@ -33,7 +34,7 @@ if event is InputEventmouseMotion and Input.get_mouse-MODE() == Input.MOUSE_MODE
 						
 						if direction:
 							velocity.x = move_toward(velocity.x, 0, SPEED)
-							velocity.z = move_towards(velocity.z, 0,SPEED)
+							velocity.z = move_toward(velocity.z, 0,SPEED)
 							
 							move_and_slide()
 						
