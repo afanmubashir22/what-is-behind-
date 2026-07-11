4cb4c25908213ac301cfe2952bb8e81eb91a773d
afanmubashir22 <afanmubashir429@gmail.com>
2026-07-11 16:39:58 +0530

    he didnt moved
    i am trying to make him move

--- player.gd
+++ player.gd
@@ -25,29 +25,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 		
 func  _physics_process(delta: float)  -> void:
-					if not is_on_floor():
-						velocity.y -= gravity * delta
-						
-						var input_dir :=Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-						var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-						
-						if direction:
-							print("Moving direction: ", direction) # <--- ADD THIS LINE TO TEST
-							velocity.x = move_toward(velocity.x, 0, SPEED)
-							velocity.z = move_toward(velocity.z, 0,SPEED)
-						else:
-							velocity.x = move_toward(velocity.x, 0, SPEED)
-							velocity.z = move_toward(velocity.z, 0, SPEED)
-							
-							move_and_slide()
-						
-						
-						
-					
-					
-					
-					
-					
-	
-				
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+		var input_dir := Input.get_vector( "ui_left", "ui_right", "ui_up", "ui_down")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if  direction:
+			velocity.x = direction.x * SPEED
+			velocity.z  = direction.z * SPEED
+			move_and_slide()
 			
+		
+		
