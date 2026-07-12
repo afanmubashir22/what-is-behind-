12e6422f92737181cbfb4295c07ff8b9640717e5
afanmubashir22 <afanmubashir429@gmail.com>
2026-07-12 15:39:43 +0530

    i made the partition like crosswall and rooms also made other things

--- player.gd
+++ player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody3D
 
-@export var SPEED: float = 4.0
-@export var MOUSE_SENSITIVITY: float = 0.002
+@export var walk_speed: float = 5.0
+@export var jump_velocity:float=4.5
+@export var mouse_sensitivity:float=0.002
+@export var acceleration:float=10.0
+@export var decleration:float=12.0
 
 @onready var head: Node3D = $Head
 @onready var camera: Camera3D = $Head/Camera3D
@@ -20,29 +23,23 @@ func _unhandled_input(event: InputEvent) -> void:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			
 	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
-		rotate_y(-event.relative.x * MOUSE_SENSITIVITY)
-		head.rotate_x(-event.relative.y * MOUSE_SENSITIVITY)
+		rotate_y(-event.relative.x * mouse_sensitivity)
+		head.rotate_x(-event.relative.y * mouse_sensitivity)
 		head.rotation.x = clamp(head.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 		
 func  _physics_process(delta: float)  -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-		var input_dir = Vector3.ZERO
-		if Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_UP):
-			input_dir -= transform.basis.z
-		if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
-			input_dir += transform.basis.z
-		if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
-			input_dir -= transform.basis.x
-		if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
-						input_dir += transform.basis.x
-						
-						var direction = input_dir.normalized()
-						
-						if direction != Vector3.ZERO:
-							velocity.x = direction.x * SPEED
-							velocity.z = direction.z * SPEED
-						else:
-								velocity.x = move_toward(velocity.x, 0, SPEED)
-								velocity.z = move_toward(velocity.z, 0, SPEED)
-							
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y=jump_velocity
+		
+	var input:=Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	
+	var direction :=(transform.basis*Vector3(input.x,0,input.y)).normalized()
+	if direction != Vector3.ZERO:
+		velocity.x = move_toward(velocity.x,direction.x*walk_speed,acceleration*delta)
+		velocity.z = move_toward(velocity.z,direction.z*walk_speed,acceleration*delta)
+	else:
+		velocity.x = move_toward(velocity.x, 0,decleration*delta)
+		velocity.z = move_toward(velocity.z, 0, decleration*delta)
+	move_and_slide()
