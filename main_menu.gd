ebc428032747316ee8457fcfe971a72bb2188c5e
afanmubashir22 <afanmubashir429@gmail.com>
2026-08-02 20:52:56 +0530

    i fixed some error runned the test but loos like there are some error left thaat is hindering theAI child to climb up the stairs lets work on the next.....

--- main_menu.gd
+++ main_menu.gd
@@ -2,12 +2,15 @@ extends Control
 @export_file("*.tscn") var main_game_scene: String = "res://house.tscn"
 @onready var play_button = $VBoxContainer/PlayButton
 @onready var quit_button = $VBoxContainer/QuitButton
+@onready var background_music = $BackgroundMusic
+
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	play_button.pressed.connect(_on_play_pressed)
 	quit_button.pressed.connect(_on_quit_pressed)
 func _on_play_pressed():
+	if background_music:
+		background_music.stop()
 	get_tree().change_scene_to_file(main_game_scene)
 func _on_quit_pressed():
 		get_tree().quit()
-		
