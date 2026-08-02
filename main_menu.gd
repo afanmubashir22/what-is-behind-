356a1cb0ebc6360838ef70d8ff86ec03f86ce78b
afanmubashir22 <afanmubashir429@gmail.com>
2026-08-02 20:02:04 +0530

    i made the main menu for the game that looks terrefic

---
+++ main_menu.gd
@@ -0,0 +1,13 @@
+extends Control
+@export_file("*.tscn") var main_game_scene: String = "res://house.tscn"
+@onready var play_button = $VBoxContainer/PlayButton
+@onready var quit_button = $VBoxContainer/QuitButton
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	play_button.pressed.connect(_on_play_pressed)
+	quit_button.pressed.connect(_on_quit_pressed)
+func _on_play_pressed():
+	get_tree().change_scene_to_file(main_game_scene)
+func _on_quit_pressed():
+		get_tree().quit()
+		
