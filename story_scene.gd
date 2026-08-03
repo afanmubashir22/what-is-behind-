596f2725941484700d47c6cdbb2327a040033a26
afanmubashir22 <afanmubashir429@gmail.com>
2026-08-04 00:56:31 +0530

    the control key wasent working but now i fixed it. and now i will ne working on the scarrychild or pick up etc

--- story_scene.gd
+++ story_scene.gd
@@ -7,11 +7,13 @@ func _ready():
 		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 		if creepy_music and not creepy_music.playing:
 			creepy_music.play()
-				
+		if not continue_button.pressed.is_connected(_on_continue_pressed):
 			continue_button.pressed.connect(_on_continue_pressed)
 func _on_continue_pressed():
 	if creepy_music:
 		creepy_music.stop()
 		
 		get_tree().change_scene_to_file(main_game_scene)
+func _on_continue_button_pressed():
+		_on_continue_pressed()
 		
