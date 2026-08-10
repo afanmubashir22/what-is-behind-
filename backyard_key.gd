c7761980f54274aaebb037ddef5c95fc7bcb6f60
afanmubashir22 <afanmubashir429@gmail.com>
2026-08-10 21:09:23 +0530

    i made a key for backyard

---
+++ backyard_key.gd
@@ -0,0 +1,13 @@
+extends Area3D
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player") or body.name == "PLAYER":
+		var door = get_node_or_null("/root/House/BackyardDoor")
+		if door:
+				door.queue_free()
+		var task_label = get_node_or_null("/root/House/CanvasLayer/Label")
+		if task_label:
+				task_label.text = "Objective:Escape through the backyard!"
+				queue_free()
+				
