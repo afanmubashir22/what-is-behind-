a5c3354c35d42fdef1b6861cb5d6524cefb175fb
afanmubashir22 <afanmubashir429@gmail.com>
2026-08-11 12:32:18 +0530

    i am fixing the key bug in the game

--- backyard_key.gd
+++ backyard_key.gd
@@ -1,26 +1,20 @@
 extends Area3D
-var player_near: bool = false
 func _ready() -> void:
-	body_entered.connect(_on_body_entered)
-	body_exited.connect(_on_body_exited)
-	
+		monitoring = true
+		body_entered.connect(_on_body_entered)
 func _on_body_entered(body: Node3D) -> void:
-		if body.is_in_group("player") or body.name == "Player":
-				player_near = true
-func _on_body_exited(body: Node3D) -> void:
-		if body.is_in_group("player") or body.name == "Player":
-				player_near = false
-func _unhandled_input(event: InputEvent) -> void:
-		if player_near and (event.is_action_pressed("ui_accept") or (event is InputEventKey and event.pressed and event.keycode == KEY_E)):
-				pick_up_key()
-func pick_up_key() -> void:
-		var door = get_node_or_null("/root/house/BackyardDoor")
+		if not (body.is_in_group("player") or body.name == "Player" or body.name == "character"):
+				return
+		var door = get_node_or_null("/root/House/BackyardDoor")
 		if door:
-				door.set_meta("is_unlocked", true)
+				door.queue_free()
 		var task_label = get_node_or_null("/root/house/CanvasLayer/Label")
 		if task_label:
-				task_label.text = "Backyard Door Unlocked!"
-				await get_tree().create_timer(1.5).timeout
 				task_label.text = "Objective: Go to the backyard"
-				queue_free()
+		var msg_label = get_node_or_null("/root/House/CanvasLayer/MessageLabel")
+		if msg_label:
+				msg_label.text = "BACKYARD DOOR UNLOCKED!"
+				await get_tree().create_timer(2.5).timeout
+				msg_label.text = ""
+		queue_free()
 				
