44fb673fea5bf2e90dd771b8c39a41f01225090a
afanmubashir22 <afanmubashir429@gmail.com>
2026-08-11 19:08:07 +0530

    its not working the key is not working but i am trying to fix it

--- backyard_key.gd
+++ backyard_key.gd
@@ -1,22 +1,15 @@
 extends Area3D
-@export var message_label: Label
+@export var objective_label: Label
 @export var House: Node3D
-func _ready() -> void:
-	if message_label:
-		message_label.hide()
 func _on_body_entered(body: Node3D) -> void:
-	if body.name =="Player":
 		_collect_key()
 func _collect_key() -> void:
-	if House:
-		House.queue_free()
-	if message_label:
-			message_label.text = "backyard door unocked!"
-			message_label.show()
-	visible = false
-	set_deferred("monitoring", false)
-	set_deferred("monitorable", false)
-	await get_tree().create_timer(2.0)
-	if message_label:
-		message_label.hide()
-		queue_free()
+		if objective_label:
+				objective_label.text = "Objective: Backyard door unlocked"
+		if House:
+					House.queue_free()
+		hide()
+		$CollisionShape3D.set_deferred("disabled", true)
+		await get_tree().create_timer(3.0).timeout
+		queue_free
+		
