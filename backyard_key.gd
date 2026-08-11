67077c422fb677b39a2903dae848357a4d4790b8
afanmubashir22 <afanmubashir429@gmail.com>
2026-08-11 14:04:33 +0530

    i am fixing the errors

--- backyard_key.gd
+++ backyard_key.gd
@@ -3,15 +3,24 @@ func _ready() -> void:
 		monitoring = true
 		body_entered.connect(_on_body_entered)
 func _on_body_entered(body: Node3D) -> void:
-		if not (body.is_in_group("player") or body.name == "Player" or body.name == "character"):
+		if body is StaticBody3D:
 				return
+		print("KEY TOUCHED BY: ", body.name)
 		var door = get_node_or_null("/root/House/BackyardDoor")
 		if door:
 				door.queue_free()
+		else:
+			door = get_node_or_null("/root/House/BackyardDoor")
+			if door:
+					door.queue_free()
 		var task_label = get_node_or_null("/root/house/CanvasLayer/Label")
+		if not task_label:
+				task_label = get_node_or_null("/root/House/CanvasLayer/Label")
 		if task_label:
-				task_label.text = "Objective: Go to the backyard"
-		var msg_label = get_node_or_null("/root/House/CanvasLayer/MessageLabel")
+			task_label.text = "Objective: Go to the Backyard"
+		var msg_label = get_node_or_null("/root/house/CanvasLayer/MessageLabel")
+		if not msg_label:
+				msg_label = get_node_or_null("/root/House/CanvasLayer/MessageLabel")
 		if msg_label:
 				msg_label.text = "BACKYARD DOOR UNLOCKED!"
 				await get_tree().create_timer(2.5).timeout
